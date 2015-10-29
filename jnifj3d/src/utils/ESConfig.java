ea737a2285a9147587654bdbdaf0bc8f2906e586
Soiz <Soiz@192.168.1.18>
2015-10-29 16:27:32 +0100

    ElderScrollsUtils project removed

---
+++ jnifj3d/src/utils/ESConfig.java
@@ -0,0 +1,19 @@
+package utils;
+
+public class ESConfig
+{
+	//NOTE fallout modular dungeon sections are 2.56 meters or 128 units each (size of a human?)
+
+	// NOTE es x,y,z to j3d x,z,-y
+
+	// 1/0.02 = 50
+	public static float ES_TO_METERS_SCALE = 0.02f;
+
+	public static String TES_TEXTURE_PATH = "Textures\\";
+
+	public static String TES_MESH_PATH = "Meshes\\";
+
+	public static String TES_SOUND_PATH = "Sound\\";
+
+
+}
