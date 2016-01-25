bc299a1843381624cc480dcc39d5085d4e6bb823
phil <phil@Thinky>
2016-01-25 18:09:18 +0100

    .

--- jnifj3d/src/nif/j3d/animation/J3dNiVisController.java
+++ jnifj3d/src/nif/j3d/animation/J3dNiVisController.java
@@ -1,59 +1,57 @@
 package nif.j3d.animation;
 
+import java.util.ArrayList;
+
+import javax.media.j3d.Material;
+import javax.media.j3d.RenderingAttributes;
+import javax.media.j3d.Shape3D;
+
+import nif.enums.TargetColor;
 import nif.j3d.J3dNiAVObject;
-import nif.j3d.J3dNiNode;
-import nif.j3d.J3dNiTriShape;
 import nif.j3d.animation.j3dinterp.interp.BoolInterpolator;
-import nif.j3d.particles.J3dNiParticleSystem;
 import nif.niobject.controller.NiVisController;
 
 public class J3dNiVisController extends J3dNiTimeController implements BoolInterpolator.Listener
 {
-	//private ModelClip modelClip;
+	private ArrayList<RenderingAttributes> targetRenderingAttributes = new ArrayList<RenderingAttributes>();
 
 	private boolean currentVis = true;
 
-	//TODO: ModelClip no longer supported called in FO3 for sure! find a new system 
-	// can't just disconnect as animation would stop, perhaps set a null appearance?
-	// F:\game media\Morrowind\Meshes\r\xascendedsleeper.nif good example
-	// but seems to be set in FO3 wasteland
+	//TODO: why is this not working for acsended sleeper in morrowind skull?
+
+	
+	// need to pull out the Appearance and use the randring attributes to set visible false
 	public J3dNiVisController(NiVisController controller, J3dNiAVObject nodeTarget)
 	{
 		super(controller, nodeTarget);
-		if (nodeTarget instanceof J3dNiNode || nodeTarget instanceof J3dNiParticleSystem || nodeTarget instanceof J3dNiTriShape)
+		for (int i = 0; i < nodeTarget.numChildren(); i++)
 		{
+			if (nodeTarget.getChild(i) instanceof Shape3D)
+			{
+				Shape3D shape = (Shape3D) nodeTarget.getChild(i);
 
-		/*	Vector4d[] planes = new Vector4d[6];
-			for (int i = 0; i < 6; i++)
-			{//Ax + By + Cz + D <= 0
-				planes[i] = new Vector4d(0, 0, 0, Double.NEGATIVE_INFINITY);
+				RenderingAttributes targetRenderingAttribute = shape.getAppearance().getRenderingAttributes();
+				if (targetRenderingAttribute == null)
+				{
+					targetRenderingAttribute = new RenderingAttributes();
+					shape.getAppearance().setRenderingAttributes(targetRenderingAttribute);
+				}
+				targetRenderingAttribute.setCapability(RenderingAttributes.ALLOW_VISIBLE_WRITE);
+				targetRenderingAttributes.add(targetRenderingAttribute);
 			}
-			modelClip = new ModelClip(planes);
-			modelClip.setInfluencingBounds(null);
-			modelClip.setCapability(ModelClip.ALLOW_INFLUENCING_BOUNDS_WRITE);
-			modelClip.addScope(nodeTarget);
-
-			nodeTarget.addChild(modelClip);*/
-
 		}
-		else
-		{
-			new Throwable("node target is not allowed for vis controller " + nodeTarget + " in " + controller.nVer.fileName)
-					.printStackTrace();
-		}
-
+		//System.out.println("J3dNiVisController");
 	}
 
 	private void setVis(boolean isVis)
 	{
-		 
-		if (isVis && !currentVis)
+//System.out.println("isVis "+isVis);
+		if (isVis != currentVis)
 		{
-//			modelClip.setInfluencingBounds(null);
-		}
-		else if (!isVis && currentVis)
-		{
-//			modelClip.setInfluencingBounds(new BoundingSphere(new Point3d(), Double.POSITIVE_INFINITY));
+			for (RenderingAttributes targetRenderingAttribute : targetRenderingAttributes)
+			{
+				targetRenderingAttribute.setVisible(isVis);
+			}
 		}
 		currentVis = isVis;
 	}
