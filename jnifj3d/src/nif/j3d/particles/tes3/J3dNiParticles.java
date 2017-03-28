8f390ca9ea8934d3bd81ea8a7d43e4f09f2ca421
phil <philjord@ihug.co.nz>
2017-03-28 13:29:37 +1300

    set the min filter for texture failed ona reused texture that's already live

--- jnifj3d/src/nif/j3d/particles/tes3/J3dNiParticles.java
+++ jnifj3d/src/nif/j3d/particles/tes3/J3dNiParticles.java
@@ -362,7 +362,9 @@ public class J3dNiParticles extends J3dNiGeometry
 						else
 						{
 							//POINT array data can't use mipmaps, texture loader default to nicest min filter
-							tex.setMinFilter(Texture.BASE_LEVEL_LINEAR);
+							if(!tex.isLive() && !tex.isCompiled())
+								tex.setMinFilter(Texture.BASE_LEVEL_LINEAR);
+							
 							TextureUnitState[] tus = new TextureUnitState[1];
 							TextureUnitState tus0 = new TextureUnitState();
 							tus0.setTexture(tex);
